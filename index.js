8b6c303625274450669625df31112a3e45702e79
João Vitor Néias <agsuns@gmail.com>
2020-10-25 15:34:46 -0300

    inicial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let eita = "essa é uma string bem lixo: ";
+let number = 7;
+let alea;
+let teste = eita;
+
+number = number.toString();
+alea = number.toString();
+console.log(eita + alea);
+console.log(number + 7);
+console.log(teste)
+
+console.log("EU SOU MTO GAY".toLowerCase());
